15db2ca1a6fdcbcf8af3ce7da8b767663659494d
unknown <rjmahir.faisal@gmail.com>
2021-10-10 18:40:18 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMkgFTMlEoR-iJq3wtU7SLmFr8sObGvIw",
+    authDomain: "ema-john-787f6.firebaseapp.com",
+    projectId: "ema-john-787f6",
+    storageBucket: "ema-john-787f6.appspot.com",
+    messagingSenderId: "742412261010",
+    appId: "1:742412261010:web:b59a71941bc63af3bef84b",
+    measurementId: "G-F7CS1YBJR9"
+};
+
+export default firebaseConfig;
